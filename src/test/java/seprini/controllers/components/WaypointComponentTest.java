ddeedc51799fa1cd3eb2ad1a88aaf2b583793ca3
James Cowgill <james410@cowgill.org.uk>
2014-02-14 13:01:33 +0000

    Added WaypointComponent tests

---
+++ src/test/java/seprini/controllers/components/WaypointComponentTest.java
@@ -0,0 +1,49 @@
+package seprini.controllers.components;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import seprini.controllers.AircraftController;
+import seprini.data.GameDifficulty;
+import seprini.models.Airspace;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test class or {@link WaypointComponent}
+ */
+@RunWith(JUnit4.class)
+public class WaypointComponentTest
+{
+	private WaypointComponent waypointComponent;
+
+	@Before
+	public void setUpWaypointComponent()
+	{
+		waypointComponent = new WaypointComponent(
+		                        new AircraftController(GameDifficulty.EASY, new Airspace(), null));
+	}
+
+	/** Should be >= 2 entry points */
+	@Test
+	public void testEntrypoints()
+	{
+		assertThat(waypointComponent.getEntryList(), hasSize(greaterThanOrEqualTo(2)));
+	}
+
+	/** Should be >= 2 exit points */
+	@Test
+	public void testExitpoints()
+	{
+		assertThat(waypointComponent.getExitList(), hasSize(greaterThanOrEqualTo(2)));
+	}
+
+	/** Should be >= 6 way points */
+	@Test
+	public void testWaypoints()
+	{
+		assertThat(waypointComponent.getPermanentList(), hasSize(greaterThanOrEqualTo(6)));
+	}
+}
