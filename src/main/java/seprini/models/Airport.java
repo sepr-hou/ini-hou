ab0fd41621c3e1210f76795c37f9fe28acc73e43
StevenWhitelock <sgw509@example.com>
2014-03-06 12:00:58 +0000

    Added temp airport graphix, Will not show up yet, will fix after ARIN!

--- src/main/java/seprini/models/Airport.java
+++ src/main/java/seprini/models/Airport.java
@@ -1,10 +1,21 @@
 package seprini.models;
 
+import com.badlogic.gdx.math.Vector2;
+
+import seprini.data.Art;
+
 public final class Airport extends Entity{
 
 	Waypoint runwayEnd = new Waypoint(464, 395, false);
 	Waypoint runwayMid = new Waypoint(387, 335, false);
 	Waypoint runwayStart = new Waypoint(310, 275, false);
+	
+	public Airport() {
+		this.coords = new Vector2(387, 335);
+		this.size = new Vector2(154, 120);
+		this.texture = Art.getTextureRegion("airport");
+	}
+	
 	
 	public Waypoint getStart(){
 		return runwayStart;
